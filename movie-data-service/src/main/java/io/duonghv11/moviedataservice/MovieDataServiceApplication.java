f3bfd096700dee9ccd72c9be9077b9af2042f301
duonghan <20155296@student.hust.edu.vn>
2020-09-22 06:59:05 +0700

    first init 3 services

---
+++ movie-data-service/src/main/java/io/duonghv11/moviedataservice/MovieDataServiceApplication.java
@@ -0,0 +1,13 @@
+package io.duonghv11.moviedataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieDataServiceApplication.class, args);
+    }
+
+}
